6632c6e970f0a1983f5f4831005ca5d689fd13e4
faizde <widifaizakhmad@gmail.com>
2023-10-03 13:44:03 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ console.log('Hallo kita akan membuat RESTful API');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
         cors: {
             origin: ['*'],
